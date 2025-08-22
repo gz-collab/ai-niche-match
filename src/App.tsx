c7a8ec5c8168560d6852f699b8bf3db5a4b54465
gz-collab <gregor.zebic@gmail.com>
2025-08-23 00:00:13 +0200

    Added App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+@@ .. @@
+ import React from "react";
+-import AINicheMatchMVP from "./components/AINicheMatchMVP";
++import AINicheMatchMVP from "./components/AINicheMatchMVP";
+
+ export default function App() {
+   return <AINicheMatchMVP />;
+ }
